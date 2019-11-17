3dec33db4c0fdf7d5c9f8e2a64f30e52e832a06e
matteov <matteo.volpi@epitech.eu>
2019-11-17 15:32:53 +0100

    [feat] It's working ffs ! : First real working version, display audio pitch with matplotlib. A lot of improvement can be made

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pydub import AudioSegment, playback
 import threading
+import time
+
+
+currentTime = lambda: int(round(time.time() * 1000))
+
 
 def plotter(tabX, tabY, line):
     if line == []:
         plt.ion()
-        fig = plt.figure(figsize=(13,6))
+        fig = plt.figure(figsize=(13, 6))
         ax = fig.add_subplot(111)
         line, = ax.plot(tabX, tabY, '-o', alpha=0.8)
         plt.show()
@@ -20,12 +25,15 @@ def plotter(tabX, tabY, line):
     if np.min(tabY) <= line.axes.get_ylim()[0] or np.max(tabY) >= line.axes.get_ylim()[1]:
         plt.ylim([np.min(tabY) - np.std(tabY),np.max(tabY) + np.std(tabY)])
 
-    plt.pause(0.01)
+    plt.pause(0.005)
     return line
 
+
 def play(file):
+    time.sleep(0.1)
     song = AudioSegment.from_wav(file)
     playback.play(song)
+
 
 def main():
     # Argument parsing
@@ -36,39 +44,52 @@ def main():
     args = parser.parse_args()
     #print(args)
 
-    # open audio file to analyze
-    s = aubio.source(args.file, hop_size = 1024)
+    # Open audio file to get samplerate and duration of the track then close it
+    s = aubio.source(args.file)
     samplerate = s.samplerate
-    hopSize = s.hop_size
-    winSize = 4096
+    duration = s.duration
+    s.close()
 
-    # length of track in seconds, divide the nubmer of samples by sumber of sample for one second
-    sourceLength = s.duration / s.samplerate
-    #print(s.duration, s.samplerate, s.duration / s.samplerate)
+    # Compute length of track in seconds, divide the total nubmer of samples by sumber of sample
+    # for one second
+    print(s.duration, samplerate, duration / samplerate)
 
-    tolerance = 0.8
+    # I want to update the plot 20 times per second, so hopSize is the number of sample aubio should send me
+    hopSize = samplerate // 20
 
-    pitchOutput = aubio.pitch("yin", winSize, hopSize, samplerate)
+    # Reopen the file with a new hopSize according to its samplerate
+    s = aubio.source(args.file, hop_size=hopSize)
+
+    pitchOutput = aubio.pitch("default", 4096, hopSize, s.samplerate)
     pitchOutput.set_unit("midi")
-    pitchOutput.set_tolerance(tolerance)
+    #pitchOutput.set_tolerance(0.8)
 
     plt.style.use("ggplot")
 
-    tabX = np.linspace(0, 1, 1025)[0:-1]
+    tabX = np.linspace(0, 1, hopSize + 1)[0:-1]
     line = []
 
+    # Play music of another thread because pydub is blocking, another method is probably better
+    # but this is what I got
     if args.p is True:
         threading.Thread(target=lambda: play(args.file)).start()
 
     while True:
         samples, read = s()
-        """n = []
-        for i in range(0, 1024, 2):
-            n.append(samples[i])"""
-        #print(len(tabX), len(samples), samplerate)
+
+        startTime = currentTime()
         line = plotter(tabX, samples, line)
+        endTime = currentTime()
+
+        diff = endTime - startTime
+        sleepTime = (50 - diff) / 1000
+
+        #print("'", sleepTime, "'")
+        time.sleep(sleepTime if sleepTime > 0 else 0)
+
         if read < hopSize:
             break
+
 
 if __name__ == "__main__":
     main()
