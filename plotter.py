41f0bf0cba86838b76ef3a9d28a2af656ece8a2c
matteov <matteo.volpi@epitech.eu>
2019-11-23 14:29:36 +0100

    [feat] pyqtgraph : fully replace mtplotlib by pyqtgraph

--- plotter.py
+++ plotter.py
@@ -5,8 +5,11 @@ import numpy as np
 
 class QtPlotter:
 
-    def __init__(self):
-        app = QtGui.QApplication([])
+    def __init__(self, sound, hopSize):
+        self.sound = sound
+        self.hopSize = hopSize
+
+        QtGui.QApplication([])
 
         self.win = pg.GraphicsWindow(title="Audio feature extractor")
         self.win.resize(1000, 600)
@@ -15,9 +18,8 @@ class QtPlotter:
 
         self.plot = self.win.addPlot(title="Pitch")
         self.curve = self.plot.plot(pen='y')
+        self.plot.setYRange(-2, 2)
         self.data = np.random.normal(size=(10, 100))
-
-        self.ptr = 0
 
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.__update)
@@ -27,17 +29,11 @@ class QtPlotter:
 
 
     def __update(self):
-        self.curve.setData(self.data[self.ptr % 10])
-        if self.ptr == 0:
-            self.plot.enableAutoRange('xy', False)
-        self.ptr += 1
-        if self.ptr > 20:
+        samples, read = self.sound()
+
+        self.curve.setData(samples)
+
+        if read < self.hopSize:
             self.timer.stop()
+            self.win.close()
 
-
-class MplPlotter:
-
-    def __init__(self):
-        pass
-
-p = QtPlotter()
